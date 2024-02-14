0b5c02306317401653eb91c04115fb6d925bbaf0
Sergiy Parkhuts <sergiy.parkhuts@andopen.co>
2024-02-14 14:39:48 +0200

    Create products#update endpoint + rspec test + routes update

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,30 @@
 # frozen_string_literal: true
 
 class ProductsController < ApplicationController
+  before_action :load_product, only: %i[update]
+
   def index
     products = Product.all
     render json: products, each_serializer: ProductSerializer
+  end
+
+  def update
+    return render json: { error: 'Product not found' }, status: :not_found unless @product
+
+    if @product.update(update_params)
+      render json: @product, status: :ok
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def update_params
+    params.permit(:price_cents, :price_currency)
+  end
+
+  def load_product
+    @product = Product.find_by(id: params[:id])
   end
 end
--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -21,4 +21,46 @@ RSpec.describe ProductsController, type: :controller do
       expect(parsed_body.pluck('id')).to match_array(Product.all.ids)
     end
   end
+
+  describe "POST 'update'" do
+    let(:target_product) { create(:product, :mug) }
+    let(:request) { post :update,  params: }
+
+    context 'when successfully updates the product' do
+      let(:params) { { id: target_product.id, price_cents: 10_00 } }
+
+      it 'returns updated product' do
+        expect(target_product.price_cents).to eq(6_00)
+
+        perform_request
+
+        expect(target_product.reload.price_cents).to eq(10_00)
+      end
+    end
+
+    context 'when product not found' do
+      let(:params) { { id: -1, price_cents: 10_00 } }
+
+      before { perform_request }
+
+      it { expect(response).to have_http_status(:not_found) }
+
+      it 'returns correct error message' do
+        expect(parsed_body['error']).to eq('Product not found')
+      end
+    end
+
+    context 'when error occurs while updating' do
+      let!(:target_product) { create(:product, :mug) }
+      let(:params) { { id: target_product.id } }
+
+      it 'returns 422 status' do
+        allow_any_instance_of(Product).to receive(:valid?).and_return(false)
+
+        perform_request
+
+        expect(response).to have_http_status(422)
+      end
+    end
+  end
 end
